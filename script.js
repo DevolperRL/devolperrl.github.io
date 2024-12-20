a1c28ac2fa3c734af089aeeb29f41a86bbe03dca
DevolperRL <100154298+DevolperRL@users.noreply.github.com>
2024-12-20 17:43:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const tutorialList = document.getElementById("tutorial-list");
 
 const tutorialsData = {
     coding: [
-        { title: "Introduction to HTML", date: "Dec 20, 2023" },
-        { title: "CSS Fundamentals", date: "Dec 18, 2023" },
+        { title: "Introduction to HTML", date: "Dec 20, 2023", link: "tutorial-example.html" },
+        { title: "CSS Fundamentals", date: "Dec 18, 2023", link: "tutorial-css.html" },
     ],
     modeling: [
-        { title: "3D Modeling Basics", date: "Dec 19, 2023" },
-        { title: "Character Design", date: "Dec 17, 2023" },
+        { title: "3D Modeling Basics", date: "Dec 19, 2023", link: "tutorial-modeling.html" },
+        { title: "Character Design", date: "Dec 17, 2023", link: "tutorial-character-design.html" },
     ],
     design: [
-        { title: "Graphic Design Principles", date: "Dec 15, 2023" },
-        { title: "Video Editing Basics", date: "Dec 14, 2023" },
+        { title: "Graphic Design Principles", date: "Dec 15, 2023", link: "tutorial-graphic-design.html" },
+        { title: "Video Editing Basics", date: "Dec 14, 2023", link: "tutorial-video-editing.html" },
     ],
 };
 
@@ -22,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
     tutorialsSection.style.display = "none";
     categoriesSection.style.display = "block";
 });
-
 
 // Show tutorials for a specific category
 function showTutorials(category) {
@@ -32,7 +31,7 @@ function showTutorials(category) {
         .map(
             (tutorial) =>
                 `<div class="forum-post">
-                    <h3><a href="#">${tutorial.title}</a></h3>
+                    <h3><a href="${tutorial.link}">${tutorial.title}</a></h3>
                     <p>Last updated: ${tutorial.date}</p>
                 </div>`
         )
